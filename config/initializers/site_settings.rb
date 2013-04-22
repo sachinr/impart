3f1ddaa122702cc1fdd62c34c22e6c4e60592362
Sachin Ranchod <sachin.ranchod@gmail.com>
2013-04-22 19:01:14 +0200

    catch initializer error if db not migrated

--- config/initializers/site_settings.rb
+++ config/initializers/site_settings.rb
@@ -1,7 +1,11 @@
-puts 'setting up site settings'
-SiteSetting.defaults.each do |name, default|
-  if SiteSetting.find_by_name(name).nil?
-    setting = SiteSetting.new(name: name, value: default)
-    setting.save
+begin
+  puts 'setting up site settings'
+  SiteSetting.defaults.each do |name, default|
+    if SiteSetting.find_by_name(name).nil?
+      setting = SiteSetting.new(name: name, value: default)
+      setting.save
+    end
   end
+rescue
+  puts 'ERROR configuring settings - please run migrations'
 end
